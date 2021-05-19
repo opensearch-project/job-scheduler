45c12adfa4f9ca69734d34a7e43d9fa8b11334d5
Tianli Feng <ftianli@amazon.com>
2021-05-19 09:13:50 -0700

    Rename namespaces from odfe to opensearch (#24)
    * Rename packages in 'src' directory
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>
    
    * Rename packages in 'spi' directory
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>
    
    * Rename packages in 'sample-extension-plugin' directory
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>
    
    * Rename package paths in build.gradle scripts
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>
    
    * Restore file under META-INF.services of the sample extension plugin directory
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>
    
    * Rename file under META-INF.services of the sample extension plugin using 'git mv'
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>
    
    * Rename package path in the file under META-INF.services
    
    Signed-off-by: Tianli Feng <ftianli@amazon.com>

---
+++ spi/src/test/java/org/opensearch/jobscheduler/spi/schedule/JobDocVersionTests.java
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+/*
+ *   Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+package org.opensearch.jobscheduler.spi.schedule;
+
+import org.opensearch.jobscheduler.spi.JobDocVersion;
+import org.opensearch.test.OpenSearchTestCase;
+import org.junit.Assert;
+
+public class JobDocVersionTests extends OpenSearchTestCase {
+
+    public void testCompareTo() {
+        // constructor parameters: primary_term, seqNo, version
+        JobDocVersion version1 = new JobDocVersion(1L, 1L, 1L);
+        Assert.assertTrue(version1.compareTo(null) > 0);
+
+        JobDocVersion version2 = new JobDocVersion(1L, 2L, 1L);
+        Assert.assertTrue(version1.compareTo(version2) < 0);
+
+        JobDocVersion version3 = new JobDocVersion(2L, 1L, 1L);
+        Assert.assertTrue(version1.compareTo(version3) < 0);
+        Assert.assertTrue(version2.compareTo(version3) > 0);
+
+        JobDocVersion version4 = new JobDocVersion(1L, 1L, 1L);
+        Assert.assertTrue(version1.compareTo(version4) == 0);
+    }
+}
