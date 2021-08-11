2fe90fc730d65062fa8ccb3a691a03791c54dcca
Anshul Mathur <ansmathu@amazon.com>
2021-08-11 10:02:07 -0700

    Updated the LockService to allow for acquiring locks on resources.
    opensearch_job_scheduler_lock.json - changed to include fields for the resource lock
    LockModel - changed to include constructors for a resource lock
    SchemaUtils - created to have functions that allow for LockService to check whether the cluster schema for the lock index is up to date.
    LockService - allows for the acquisition of resource locks and also makes calls to use SchemaUtils before writing to the lock index.
    LockServiceIT - extends tests to make sure that resource locks are tested as rigorously as job locks.

---
+++ spi/src/main/java/org/opensearch/jobscheduler/spi/utils/SchemaUtils.java
@@ -0,0 +1,111 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+package org.opensearch.jobscheduler.spi.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.action.ActionListener;
+import org.opensearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.opensearch.action.support.master.AcknowledgedResponse;
+import org.opensearch.client.IndicesAdminClient;
+import org.opensearch.cluster.ClusterState;
+import org.opensearch.cluster.metadata.IndexMetadata;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.xcontent.LoggingDeprecationHandler;
+import org.opensearch.common.xcontent.NamedXContentRegistry;
+import org.opensearch.common.xcontent.XContentParser;
+import org.opensearch.common.xcontent.XContentParser.Token;
+import org.opensearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchemaUtils {
+    private static final String _META = "_meta";
+    private static final String SCHEMA_VERSION = "schema_version";
+    private static final String _DOC = "_doc";
+    private static final String LOCK_INDEX_NAME = ".opendistro-job-scheduler-lock";
+    private static final long DEFAULT_SCHEMA_VERSION = 1L;
+    private static final Logger logger = LogManager.getLogger(SchemaUtils.class);
+
+    public static long schemaVersion(String mapping) throws IOException {
+        XContentParser xcp = XContentType.JSON.xContent().createParser(NamedXContentRegistry.EMPTY,
+                LoggingDeprecationHandler.INSTANCE, mapping);
+        while (!xcp.isClosed()) {
+            Token token = xcp.currentToken();
+            if (token != null && token != Token.END_OBJECT && token != Token.START_OBJECT) {
+                if (!xcp.currentName().equals(_META)) {
+                    xcp.nextToken();
+                    xcp.skipChildren();
+                } else {
+                    while (xcp.nextToken() != Token.END_OBJECT) {
+                        if (xcp.currentName().equals(SCHEMA_VERSION)) {
+                            // throw new IOException(xcp.currentName() + xcp.currentToken());
+                            return xcp.longValue();
+                        }
+                        else {
+                            xcp.nextToken();
+                        }
+                    }
+                }
+            }
+            xcp.nextToken();
+        }
+        return DEFAULT_SCHEMA_VERSION;
+    }
+
+    public static boolean shouldUpdateIndex(IndexMetadata index, long newVersion) {
+        long oldVersion = DEFAULT_SCHEMA_VERSION;
+        Map<String, Object> indexMapping = index.mapping().sourceAsMap();
+        if (indexMapping != null && indexMapping.containsKey(_META)) {
+            Object meta = indexMapping.get(_META);
+            if (meta instanceof HashMap) {
+                HashMap<?, ?> metaData = (HashMap<?, ?>) meta;
+                if (metaData.containsKey(SCHEMA_VERSION)) {
+                    Object oldVersionObject = metaData.get(SCHEMA_VERSION);
+                    if (oldVersionObject instanceof Long) {
+                        oldVersion = (Long) oldVersionObject;
+                    }
+                    if (oldVersionObject instanceof Integer) {
+                        Integer version = (Integer) oldVersionObject;
+                        oldVersion = version.longValue();
+                    }
+                }
+            }
+        }
+        return newVersion > oldVersion;
+    }
+
+
+    public static void checkAndUpdateLockIndexMapping(String mapping, ClusterService clusterService,
+                                                      IndicesAdminClient client, ActionListener<AcknowledgedResponse> listener) {
+        ClusterState clusterState = clusterService.state();
+        if (clusterState.metadata().indices().containsKey(LOCK_INDEX_NAME)) {
+            try {
+                long newSchemaVersion = schemaVersion(mapping);
+                IndexMetadata indexMetadata = clusterState.metadata().indices().get(SchemaUtils.LOCK_INDEX_NAME);
+                if (shouldUpdateIndex(indexMetadata, newSchemaVersion)) {
+                    PutMappingRequest putMappingRequest = new PutMappingRequest(SchemaUtils.LOCK_INDEX_NAME).type(_DOC).source(mapping, XContentType.JSON);
+                    client.putMapping(putMappingRequest, listener);
+                } else {
+                    listener.onResponse(new AcknowledgedResponse(true));
+                }
+            } catch (Exception e) {
+                listener.onResponse(new AcknowledgedResponse(false));
+            }
+        } else {
+            listener.onResponse(new AcknowledgedResponse(false));
+        }
+    }
+
+}
