c07db5ce802a9baf770fe6e6e426ef993a879acb
Joshua Palis <jpalis@amazon.com>
2023-02-10 12:46:27 -0800

    [Extensions] Exposes a GetLock REST API to enable extensions to acquire a lock model for their job execution (#311)
    * Added RestGetLockAction API, added AcquireLockRequest, enables extensions to retrieve a lock model object to run their Job
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Adding Lock ID field to RestGetLockAction response
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Added multi node integration tests for GetLockApi
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Making lock service in RestGetLockAction private
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Added Rest integration tests for RestGetLockAction
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Addressing PR comments
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Updating get lock rest path in tests
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    * Addressing PR comments
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>
    
    ---------
    
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

---
+++ src/main/java/org/opensearch/jobscheduler/transport/AcquireLockRequest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.jobscheduler.transport;
+
+import java.io.IOException;
+import java.util.Objects;
+import org.opensearch.action.ActionRequest;
+import org.opensearch.action.ActionRequestValidationException;
+import org.opensearch.common.io.stream.StreamInput;
+import org.opensearch.common.io.stream.StreamOutput;
+import org.opensearch.common.xcontent.XContentParser;
+import org.opensearch.common.xcontent.XContentParserUtils;
+
+/**
+ * Request from extensions to acquire a lock for scheduled job execution
+ */
+public class AcquireLockRequest extends ActionRequest {
+
+    /**
+     * the id of the job
+     */
+    private final String jobId;
+
+    /**
+     * the name of the job index
+     */
+    private final String jobIndexName;
+
+    /**
+     * the duration for which this lock will be acquired
+     */
+    private final long lockDurationSeconds;
+
+    public static final String JOB_ID = "job_id";
+    public static final String JOB_INDEX_NAME = "job_index_name";
+    public static final String LOCK_DURATION_SECONDS = "lock_duration_seconds";
+
+    /**
+     * Instantiates a new AcquireLockRequest
+     *
+     * @param jobId the id of the job in which the lock will be given to
+     * @param jobIndexName the name of the job index
+     * @param lockDurationSeconds the duration for which this lock will be acquired
+     */
+    public AcquireLockRequest(String jobId, String jobIndexName, long lockDurationSeconds) {
+        super();
+        this.jobId = Objects.requireNonNull(jobId);
+        this.jobIndexName = Objects.requireNonNull(jobIndexName);
+        this.lockDurationSeconds = Objects.requireNonNull(lockDurationSeconds);
+    }
+
+    /**
+     * Instantiates a new AcquireLockRequest from {@link StreamInput}
+     *
+     * @param in is the byte stream input used to de-serialize the message.
+     * @throws IOException IOException when message de-serialization fails.
+     */
+    public AcquireLockRequest(StreamInput in) throws IOException {
+        super(in);
+        this.jobId = in.readString();
+        this.jobIndexName = in.readString();
+        this.lockDurationSeconds = in.readLong();
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        out.writeString(this.jobId);
+        out.writeString(this.jobIndexName);
+        out.writeLong(this.lockDurationSeconds);
+    }
+
+    public String getJobId() {
+        return this.jobId;
+    }
+
+    public String getJobIndexName() {
+        return this.jobIndexName;
+    }
+
+    public long getLockDurationSeconds() {
+        return this.lockDurationSeconds;
+    }
+
+    @Override
+    public ActionRequestValidationException validate() {
+        return null;
+    }
+
+    public static AcquireLockRequest parse(XContentParser parser) throws IOException {
+
+        String jobId = null;
+        String jobIndexName = null;
+        Long lockDurationSeconds = null;
+
+        XContentParserUtils.ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.currentToken(), parser);
+        while (parser.nextToken() != XContentParser.Token.END_OBJECT) {
+            String fieldName = parser.currentName();
+            parser.nextToken();
+
+            switch (fieldName) {
+                case JOB_ID:
+                    jobId = parser.text();
+                    break;
+                case JOB_INDEX_NAME:
+                    jobIndexName = parser.text();
+                    break;
+                case LOCK_DURATION_SECONDS:
+                    lockDurationSeconds = parser.longValue();
+                    break;
+                default:
+                    parser.skipChildren();
+                    break;
+            }
+        }
+        return new AcquireLockRequest(jobId, jobIndexName, lockDurationSeconds);
+    }
+
+}
---
+++ src/test/java/org/opensearch/jobscheduler/multinode/GetLockMultiNodeRestIT.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package org.opensearch.jobscheduler.multinode;
+
+import com.google.common.collect.ImmutableMap;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.opensearch.client.Response;
+import org.opensearch.jobscheduler.ODFERestTestCase;
+import org.opensearch.jobscheduler.TestHelpers;
+import org.opensearch.jobscheduler.spi.LockModel;
+import org.opensearch.test.OpenSearchIntegTestCase;
+
+@OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE, numDataNodes = 2)
+public class GetLockMultiNodeRestIT extends ODFERestTestCase {
+
+    private String initialJobId;
+    private String initialJobIndexName;
+    private Response initialGetLockResponse;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        this.initialJobId = "testJobId";
+        this.initialJobIndexName = "testJobIndexName";
+        // Send initial request to ensure lock index has been created
+        this.initialGetLockResponse = TestHelpers.makeRequest(
+            client(),
+            "GET",
+            TestHelpers.GET_LOCK_BASE_URI,
+            ImmutableMap.of(),
+            TestHelpers.toHttpEntity(generateRequestBody(this.initialJobIndexName, this.initialJobId)),
+            null
+        );
+    }
+
+    public void testGetLockRestAPI() throws Exception {
+
+        String initialLockId = validateResponseAndGetLockId(entityAsMap(this.initialGetLockResponse));
+        assertEquals(generateExpectedLockId(initialJobIndexName, initialJobId), initialLockId);
+
+        // Submit 10 requests to generate new lock models for different job indexes
+        for (int i = 0; i < 10; i++) {
+            Response getLockResponse = TestHelpers.makeRequest(
+                client(),
+                "GET",
+                TestHelpers.GET_LOCK_BASE_URI,
+                ImmutableMap.of(),
+                TestHelpers.toHttpEntity(generateRequestBody(String.valueOf(i), String.valueOf(i))),
+                null
+            );
+
+            String lockId = validateResponseAndGetLockId(entityAsMap(getLockResponse));
+            assertEquals(generateExpectedLockId(String.valueOf(i), String.valueOf(i)), lockId);
+        }
+    }
+
+    private String validateResponseAndGetLockId(Map<String, Object> responseMap) {
+        assertEquals("success", responseMap.get("response"));
+        return (String) responseMap.get(LockModel.LOCK_ID);
+    }
+
+    private String generateRequestBody(String jobIndexName, String jobId) {
+        return "{\"job_id\":\"" + jobId + "\",\"job_index_name\":\"" + jobIndexName + "\",\"lock_duration_seconds\":\"30.0\"}";
+    }
+
+    private String generateExpectedLockId(String jobIndexName, String jobId) {
+        return jobIndexName + "-" + jobId;
+    }
+
+}
