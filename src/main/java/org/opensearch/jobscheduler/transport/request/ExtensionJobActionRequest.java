b48f7cffe9085d7e15e88a1f19d2786d6f11691c
Joshua Palis <jpalis@amazon.com>
2023-03-27 12:18:10 -0700

    Fixes serde logic for proxy scheduled jobrunner/parser requests/responses (#349)
    Signed-off-by: Joshua Palis <jpalis@amazon.com>

--- src/main/java/org/opensearch/jobscheduler/transport/request/ExtensionJobActionRequest.java
+++ src/main/java/org/opensearch/jobscheduler/transport/request/ExtensionJobActionRequest.java
@@ -9,15 +9,22 @@
 package org.opensearch.jobscheduler.transport.request;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import org.opensearch.common.bytes.BytesReference;
+import org.opensearch.common.io.stream.BytesStreamOutput;
 import org.opensearch.common.io.stream.Writeable;
 import org.opensearch.extensions.action.ExtensionActionRequest;
-import org.opensearch.jobscheduler.utils.JobDetailsService;
 
 /**
  * Request to extensions to invoke a job action, converts request params to a byte array
  *
  */
 public class ExtensionJobActionRequest<T extends Writeable> extends ExtensionActionRequest {
+
+    public static final byte UNIT_SEPARATOR = (byte) '\u001F';
 
     /**
      * Instantiates a new ExtensionJobActionRequest
@@ -27,7 +34,63 @@ public class ExtensionJobActionRequest<T extends Writeable> extends ExtensionAct
      * @throws IOException if serialization fails
      */
     public ExtensionJobActionRequest(String extensionActionName, T actionParams) throws IOException {
-        super(extensionActionName, JobDetailsService.convertParamsToBytes(actionParams));
+        super(extensionActionName, convertParamsToBytes(actionParams));
+    }
+
+    /**
+     * Finds the index of the specified byte value within the given byte array
+     *
+     * @param bytes the byte array to process
+     * @param value the byte to identify index of
+     * @return the index of the byte value
+     */
+    private static int indexOf(byte[] bytes, byte value) {
+        for (int offset = 0; offset < bytes.length; ++offset) {
+            if (bytes[offset] == value) {
+                return offset;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Trims off the fully qualified request class name bytes and null byte from the ExtensionActionRequest requestBytes
+     *
+     * @param requestBytes the request bytes of an ExtensionActionRequest
+     * @return the trimmed array of bytes
+     */
+    public static byte[] trimRequestBytes(byte[] requestBytes) {
+        int nullPos = indexOf(requestBytes, ExtensionJobActionRequest.UNIT_SEPARATOR);
+        return Arrays.copyOfRange(requestBytes, nullPos + 1, requestBytes.length);
+    }
+
+    /**
+     * Converts an object of type T that extends {@link Writeable} into a byte array and prepends the fully qualified class name bytes
+     *
+     * @param <T> a class that extends writeable
+     * @param actionParams the action parameters to be serialized
+     * @throws IOException if serialization fails
+     * @return the byte array of the parameters
+     */
+    private static <T extends Writeable> byte[] convertParamsToBytes(T actionParams) throws IOException {
+
+        // Write inner request to output stream and convert to byte array
+        BytesStreamOutput out = new BytesStreamOutput();
+        actionParams.writeTo(out);
+        out.flush();
+        byte[] requestBytes = BytesReference.toBytes(out.bytes());
+
+        // Convert fully qualifed class name to byte array
+        byte[] requestClassBytes = ExtensionActionRequest.class.getName().getBytes(StandardCharsets.UTF_8);
+
+        // Generate ExtensionActionRequest responseByte array
+        byte[] proxyRequestBytes = ByteBuffer.allocate(requestClassBytes.length + 1 + requestBytes.length)
+            .put(requestClassBytes)
+            .put(ExtensionJobActionRequest.UNIT_SEPARATOR)
+            .put(requestBytes)
+            .array();
+
+        return proxyRequestBytes;
     }
 
 }
