2c329326d6e8166f59f45b27f5e8667be92f9021
Jeremy Dupras <jdupras@amazon.com>
2025-06-24 12:12:15 -0700

    removing unnessessary tests
    Signed-off-by: Jeremy Dupras <jdupras@amazon.com>

--- src/test/java/org/opensearch/jobscheduler/multinode/GetScheduledInfoActionMultiNodeRestIT.java
+++ src/test/java/org/opensearch/jobscheduler/multinode/GetScheduledInfoActionMultiNodeRestIT.java
@@ -19,7 +19,7 @@ import org.opensearch.jobscheduler.ODFERestTestCase;
 import org.opensearch.jobscheduler.TestHelpers;
 import org.opensearch.test.OpenSearchIntegTestCase;
 
-@OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE, numDataNodes = 4)
+@OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE, numDataNodes = 2)
 public class GetScheduledInfoActionMultiNodeRestIT extends ODFERestTestCase {
 
     private static final String SCHEDULER_INFO_URI = "/_plugins/_job_scheduler/info";
@@ -31,23 +31,6 @@ public class GetScheduledInfoActionMultiNodeRestIT extends ODFERestTestCase {
 
         Map<String, Object> healthMap = parseResponseAsMap(healthResponse);
         assertEquals("green", healthMap.get("status"));
-        // assertEquals(4, healthMap.get("number_of_nodes"));
-
-        Response nodesResponse = TestHelpers.makeRequest(client(), "GET", "/_nodes", Map.of(), null, null);
-
-        Map<String, Object> nodesMap = parseResponseAsMap(nodesResponse);
-
-        // Print node information including ports
-        Map<String, Object> nodes = (Map<String, Object>) nodesMap.get("nodes");
-        System.out.println("Number of nodes: " + nodes.size());
-
-        for (String nodeId : nodes.keySet()) {
-            Map<String, Object> nodeInfo = (Map<String, Object>) nodes.get(nodeId);
-            Map<String, Object> http = (Map<String, Object>) nodeInfo.get("http");
-            if (http != null) {
-                System.out.println("Node " + nodeId + " HTTP address: " + http.get("publish_address"));
-            }
-        }
 
         // Test with default parameters
         Response response = TestHelpers.makeRequest(client(), "GET", SCHEDULER_INFO_URI, Map.of(), null, null);
