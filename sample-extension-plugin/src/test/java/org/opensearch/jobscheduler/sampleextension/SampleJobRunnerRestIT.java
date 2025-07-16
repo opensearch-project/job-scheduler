a7d3f17ca0282038a09daddc851a5597a87a5fd0
Jeremy Dupras <jdupras@amazon.com>
2025-07-16 15:01:38 -0700

    updating test
    Signed-off-by: Jeremy Dupras <jdupras@amazon.com>

--- sample-extension-plugin/src/test/java/org/opensearch/jobscheduler/sampleextension/SampleJobRunnerRestIT.java
+++ sample-extension-plugin/src/test/java/org/opensearch/jobscheduler/sampleextension/SampleJobRunnerRestIT.java
@@ -326,10 +326,10 @@ public class SampleJobRunnerRestIT extends SampleExtensionIntegTestCase {
     Function<Map<String, Object>, Boolean> navigationFunctionByNode = (responseJson) -> {
         List<Map<String, Object>> nodes = (List<Map<String, Object>>) responseJson.get("nodes");
         for (Map<String, Object> node : nodes) {
-            Map<String, Object> scheduled_job_info = (Map<String, Object>) node.get("scheduled_job_info");
-            List<Map<String, Object>> jobs = (List<Map<String, Object>>) scheduled_job_info.get("jobs");
-            if (!jobs.isEmpty()) {
-                Map<String, Object> job = jobs.get(0);
+            List<Map<String, Object>> nodeJobs = (List<Map<String, Object>>) ((Map<String, Object>) node.get("scheduled_job_info")).get(
+                    "jobs");
+            if (!nodeJobs.isEmpty()) {
+                Map<String, Object> job = nodeJobs.get(0);
                 List<Object> lockProperties = (List<Object>) job.get("lock");
                 Map<String, Object> lockMap = (Map<String, Object>) lockProperties.getFirst();
                 return (boolean) lockMap.get("released");
